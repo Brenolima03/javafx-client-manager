42feb25ab6212ad23924f4a611fa1d8bd84787fb
Breno Lima <brenocslima@outlook.com>
2025-01-24 23:05:13 -0400

    removing useless comments and fixing others

--- src/main/java/com/utils/CommissionReportBuilder.java
+++ src/main/java/com/utils/CommissionReportBuilder.java
@@ -3,11 +3,14 @@ package com.utils;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Map;
+
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 import com.gui.Alerts;
+
 import javafx.scene.control.Alert.AlertType;
 
 public class CommissionReportBuilder {
@@ -20,25 +23,22 @@ public class CommissionReportBuilder {
     Workbook workbook = new XSSFWorkbook();
     Sheet sheet = workbook.createSheet("Comissões");
 
-    // Create header row
     Row headerRow = sheet.createRow(0);
     headerRow.createCell(0).setCellValue("Locador");
     headerRow.createCell(1).setCellValue("Comissão");
 
     int rowIndex = 1;
 
-    // Fill the sheet with the data from the HashMap
+    // Fill the sheet with the data from the LinkedHashMap
     for (Map.Entry<String, String> entry : commissionData.entrySet()) {
       Row row = sheet.createRow(rowIndex++);
       row.createCell(0).setCellValue(entry.getKey());
       row.createCell(1).setCellValue(entry.getValue());
     }
 
-    // Auto-size columns
     sheet.autoSizeColumn(0);
     sheet.autoSizeColumn(1);
 
-    // Write to file
     try (FileOutputStream fileOut = new FileOutputStream("comissões.xlsx")) {
       workbook.write(fileOut);
       Alerts.showAlert(
