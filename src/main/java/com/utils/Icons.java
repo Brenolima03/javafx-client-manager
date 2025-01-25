4faee7904b708287b3a85435520ac454ab655cf4
Breno Lima <brenocslima@outlook.com>
2025-01-24 22:58:10 -0400

    moving transparent button creation logic to Icons class

--- src/main/java/com/utils/Icons.java
+++ src/main/java/com/utils/Icons.java
@@ -2,6 +2,8 @@ package com.utils;
 
 import java.io.File;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -23,5 +25,15 @@ public class Icons {
       ImageView iconView = new ImageView(icon);
       button.setGraphic(iconView);
     }
+  }
+
+  public static void setupTransparentButton(
+    Button button, String path, EventHandler<ActionEvent> event
+  ) {
+    Icons.setButtonIcon(button, path);
+    button.setPrefWidth(38);
+    button.setPrefHeight(38);
+    button.setOnAction(event);
+    button.setStyle("-fx-background-color: transparent; -fx-padding: 0;");
   }
 }
