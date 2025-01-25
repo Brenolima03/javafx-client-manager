07945d502027a8e8248cbaf2cad03ca752f32d06
Breno Lima <brenocslima@outlook.com>
2025-01-24 22:56:18 -0400

    return state list directly instead of modifying the combobox

--- src/main/java/com/utils/States.java
+++ src/main/java/com/utils/States.java
@@ -2,16 +2,13 @@ package com.utils;
 
 import java.util.List;
 
-import javafx.scene.control.ComboBox;
-
 public class States {
-    public static void populateStateCombobox(ComboBox stateField) {
+    public static List<String> getAllStates() {
     List<String> states = List.of(
       "AC", "AL", "AM", "AP", "BA", "CE", "DF", "ES", "GO",
       "MA", "MG", "MS", "MT", "PA", "PB", "PE", "PI", "PR",
       "RJ", "RN", "RO", "RR", "RS", "SC", "SE", "SP", "TO"
     );
-    stateField.getItems().addAll(states);
-    stateField.setPromptText("Selecione o estado");
+    return states;
   }
 }
